5e1cdcf73c1d90480c90f1c036f68fa3eeaf3c0e
adbsin <adibftw@gmail.com>
2026-01-06 11:05:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function playMusic() {
+    const music = document.getElementById("bg-music");
+    music.play();
+}
+function pauseMusic() {
+    const music = document.getElementById("bg-music");
+    music.pause();
+}
